c71de10c00b1740c38c7bc6c8f2b3800e5eb0e94
Riyazahmed K <riyazahmed.k@in.abb.com>
2021-03-24 07:26:54 +0530

    added model and DbContext class

---
+++ ToDoItemsAPI/Models/ToDoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoItemsAPI.Models
+{
+    public class ToDoContext:DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options):base(options)
+        {
+
+        }
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
