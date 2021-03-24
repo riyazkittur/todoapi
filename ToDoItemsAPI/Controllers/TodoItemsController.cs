bed5d05791295f4aa55848e158927277fb747ab1
Riyazahmed K <riyazahmed.k@in.abb.com>
2021-03-24 10:50:44 +0530

    added ToDoItemDTO and modified actions to use DTO

--- ToDoItemsAPI/Controllers/TodoItemsController.cs
+++ ToDoItemsAPI/Controllers/TodoItemsController.cs
@@ -20,14 +20,15 @@ namespace ToDoItemsAPI.Controllers
 
         // GET: api/TodoItems
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        public async Task<ActionResult<IEnumerable<TodoItemDTO>>> GetTodoItems()
         {
-            return await _context.TodoItems.ToListAsync();
+            return await _context.TodoItems
+                .Select(x=>ItemToDTO(x)).ToListAsync();
         }
 
         // GET: api/TodoItems/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        public async Task<ActionResult<TodoItemDTO>> GetTodoItem(long id)
         {
             var todoItem = await _context.TodoItems.FindAsync(id);
 
@@ -36,20 +37,27 @@ namespace ToDoItemsAPI.Controllers
                 return NotFound();
             }
 
-            return todoItem;
+            return ItemToDTO(todoItem);
         }
 
         // PUT: api/TodoItems/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutTodoItem(long id, TodoItem todoItem)
+        public async Task<IActionResult> PutTodoItem(long id, TodoItemDTO todoItemDTO)
         {
-            if (id != todoItem.Id)
+            if (id != todoItemDTO.Id)
             {
                 return BadRequest();
             }
+            var todoItem = await _context.TodoItems.FindAsync(id);
 
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+            todoItem.Name = todoItemDTO.Name;
+            todoItem.IsComplete = todoItemDTO.IsComplete;
             _context.Entry(todoItem).State = EntityState.Modified;
 
             try
@@ -75,12 +83,17 @@ namespace ToDoItemsAPI.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
-        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItemDTO todoItemDTO)
         {
+            var todoItem = new TodoItem()
+            {
+                Name = todoItemDTO.Name,
+                IsComplete = todoItemDTO.IsComplete
+            };
             _context.TodoItems.Add(todoItem);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItemDTO.Id }, ItemToDTO(todoItem));
         }
 
         // DELETE: api/TodoItems/5
@@ -103,5 +116,12 @@ namespace ToDoItemsAPI.Controllers
         {
             return _context.TodoItems.Any(e => e.Id == id);
         }
+        private static TodoItemDTO ItemToDTO(TodoItem todoItem) =>
+      new TodoItemDTO
+      {
+          Id = todoItem.Id,
+          Name = todoItem.Name,
+          IsComplete = todoItem.IsComplete
+      };
     }
 }
